b7c534fc0ff3a4dafa657c7dddaf0d0ddc65e85c
Nuno M <greven.pt@gmail.com>
2015-07-11 18:01:04 +0100

    Changed webserver and added livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,45 @@
 var gulp  = require('gulp'),
-    merge = require('merge2'),
-    uglify = require('gulp-uglify'),
-    prettyData = require('gulp-pretty-data'),
-    ui5Preload = require('gulp-openui5-preload'),
-    jshint = require('gulp-jshint'),
-    sass = require('gulp-sass');
+merge = require('merge2'),
+uglify = require('gulp-uglify'),
+prettyData = require('gulp-pretty-data'),
+ui5Preload = require('gulp-openui5-preload'),
+jshint = require('gulp-jshint'),
+sass = require('gulp-sass'),
+connect = require('gulp-connect');
 
+
+// Default task
+gulp.task('default', ['webserver', 'livereload', 'watch'], function() {
+    gulp.start('preload');
+    gulp.start('build-css');
+});
 
 // JShint task
 gulp.task('jshint', function() {
-  return gulp.src('WebContent/**/*.js')
+    return gulp.src('WebContent/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
+// Gulp Webserver
+gulp.task('webserver', function() {
+    connect.server({
+        root: 'WebContent',
+        port: 8000,
+        livereload: true
+    });
+});
+
+// Watch for changes in the CSS and refresh browser
+gulp.task('livereload', function() {
+    gulp.src(['WebContent/*.html', 'WebContent/*.js', 'WebContent/**/*.js',
+    'WebContent/**/*.css', 'WebContent/**/*.xml', 'WebContent/**/*.json'])
+    .pipe(connect.reload());
+});
+
 // Precompile SASS to CSS
 gulp.task('build-css', function() {
-  return gulp.src('WebContent/css/scss/*.scss')
+    gulp.src('WebContent/css/scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('WebContent/css'));
 });
@@ -45,17 +68,11 @@ gulp.task('preload', function() {
             'WebContent/**/*.view.json',
             'WebContent/**/*.properties'
         ])
-        )
-        .pipe(ui5Preload({
-            prefix: ''
-        }))
-        .pipe(gulp.dest('./WebContent'));
-});
-
-// Default task
-gulp.task('default', ['watch'], function() {
-    gulp.start('preload');
-    gulp.start('build-css');
+    )
+    .pipe(ui5Preload({
+        prefix: ''
+    }))
+    .pipe(gulp.dest('./WebContent'));
 });
 
 // Configure which files to watch and what tasks to use on file changes
